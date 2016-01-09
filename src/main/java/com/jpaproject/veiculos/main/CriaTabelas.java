a18d5d9715fd07e984174ee80b8ac717f40e510a
LucasES <lucas.compufc@gmail.com>
2016-01-09 20:19:18 -0300

    Conexão MySQL e JPA persistindo.

--- src/main/java/com/jpaproject/veiculos/main/CriaTabelas.java
+++ src/main/java/com/jpaproject/veiculos/main/CriaTabelas.java
@@ -1,8 +1,14 @@
 package com.jpaproject.veiculos.main;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
 public class CriaTabelas {
 	public static void main(String[] args) {
-		Persistence.createEntityManagerFactory("AlgaWorksPU");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("AlgaWorksPU");
+		EntityManager em = emf.createEntityManager();
+				
+		//em.close();
 	}
 }
